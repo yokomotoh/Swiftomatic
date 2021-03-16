110ed5854a62bd74bfdc257743654a5b2e50fdce
SPIRIIT-hub <baptistemoulin18@gmail.com>
2021-03-16 14:52:12 +0100

    debut du projet

---
+++ Swiftomatic/Swiftomatic/SwiftomaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftomaticApp.swift
+//  Swiftomatic
+//
+//  Created by Baptiste Moulin on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftomaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
